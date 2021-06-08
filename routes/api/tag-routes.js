194da96dd68a5d54f7f88e5fed684d2b0e46b418
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-06-08 15:51:32 -0600

    add, update, and delete tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -50,14 +50,36 @@ router.get('/:id', (req, res) => {
 
   // create a new tag
 router.post('/', (req, res) => {
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    res.status(500).json({ message: 'Tag Not Created'});
+  });
 });
 
 // update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
+  Tag.update(
+    {tag_name: req.body.tag_name},
+    {where: {id: req.params.id}}
+  )
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    res.status(500).json({ message: 'Tag Not Updated'});
+  });
 });
 
 // delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
+  Tag.destroy(
+    {where: {id: req.params.id}}
+  )
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    res.status(500).json({ message: 'Tag Not Deleted'});
+  });
 });
 
 module.exports = router;
